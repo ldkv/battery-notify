f2fcb6d672603caaf5a4927fdb298608d8837701
Vu LE <ledkvu@gmail.com>
2024-10-07 23:40:04 +0200

    Move assets into source folder

--- battery_notifier/configs.py
+++ battery_notifier/configs.py
@@ -8,7 +8,7 @@ NOTIFICATION_THRESHOLD = 20
 
 def resource_path(relative_path: str) -> Path:
     """Get absolute path to resource, works for dev and for PyInstaller"""
-    current_path = Path(__file__).parent.parent
+    current_path = Path(__file__).parent
     base_path = getattr(sys, "_MEIPASS", current_path.absolute())
     return base_path / relative_path
 
