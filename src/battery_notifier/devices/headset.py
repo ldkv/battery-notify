3e532393d30a1aa5804f2c3a45e11fa40cb19d02
Vu LE <daokhanhvu.le@veesion.com>
2025-03-23 22:09:41 +0100

    Add new field `report_descriptor` to `DeviceInfo`

--- src/battery_notifier/devices/headset.py
+++ src/battery_notifier/devices/headset.py
@@ -9,7 +9,10 @@ READ_TIMEOUT_MS = 1000
 class Headset(BaseDevice):
     VID: int = 0x03F0
     PID: int = 0x098D
-    matching_device: DeviceInfo = DeviceInfo(usage=514)
+    device_info: DeviceInfo = DeviceInfo(
+        usage=514,
+        report_descriptor="06 43 FF 0A 02 02 A1 01 85 21 95 1E 75 08 15 00 26 FF 00 09 00 81 02 09 00 91 02 C0 05 0C 09 01 A1 01 85 01 15 00 25 01 05 0C 09 CD 09 E9 09 EA 09 B5 09 B6 09 E2 95 06 75 01 81 02 75 02 95 01 81 03 C0",
+    )
     battery_index: int = 3
     name: str = "HyperX Cloud Alpha Wireless"
     type: str = "Headset"
@@ -30,4 +33,13 @@ class Headset(BaseDevice):
             return DEFAULT_BATTERY_LEVEL
 
         report = device.read(self.battery_index + 1, READ_TIMEOUT_MS)
+        if len(report) < self.battery_index + 1:
+            return DEFAULT_BATTERY_LEVEL
+
         return report[self.battery_index]
+
+
+if __name__ == "__main__":
+    headset = Headset()
+    battery_level = headset.update_battery_level()
+    print(f"{headset.name} - Battery level: {battery_level}")
